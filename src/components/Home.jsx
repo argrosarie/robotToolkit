3e7b93333b0b09c4c0983b766c9126347b91e61f
rosarie <rosariosanchezsampietro@gmail.com>
2023-09-22 10:50:53 -0300

    productSlice

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
-import productList from "../data/productList.json";
 import cartSlice from "../data/cartSlice";
 import "../styles/home.scss";
+import { fetchAllProducts } from "../data/productSlice";
+import { useEffect } from "react";
 
 const Home = () => {
-  const { cartProductIds } = useSelector((state) => state.cart);
+  const state = useSelector((state) => state);
+  const { cart, products } = state;
   const { addToCart, removeFromCart } = cartSlice.actions;
   const dispatch = useDispatch();
-  console.log(cartProductIds);
+
+  useEffect(() => {
+    dispatch(fetchAllProducts("http://localhost:300/products"));
+  }, [dispatch]);
 
   return (
     <div className="container product-catalogue">
       <div className="row">
-        {productList.products.map((product) => {
+        {products.data?.map((product) => {
           return (
             <div className="wrapper col-md-4" key={product.id}>
               <div className="card">
@@ -26,7 +31,7 @@ const Home = () => {
                   <h5 className="card-title">{product.name}</h5>
                   <p className="card-text">${product.price}</p>
 
-                  {!cartProductIds.includes(product.id) && (
+                  {!cart.cartProductIds.includes(product.id) && (
                     <button
                       className="btn btn-primary"
                       onClick={() => dispatch(addToCart(product.id))}
@@ -34,7 +39,7 @@ const Home = () => {
                       Add to cart
                     </button>
                   )}
-                  {cartProductIds.includes(product.id) && (
+                  {cart.cartProductIds.includes(product.id) && (
                     <button
                       className="btn btn-primary"
                       onClick={() => dispatch(removeFromCart(product.id))}
